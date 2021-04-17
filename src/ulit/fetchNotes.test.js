7939c48d9369b4c829c25604329630934de75384
Roody <rjointer2@gmail.com>
2021-04-17 14:19:56 -0700

    removed test, will explore later. time for production

--- src/ulit/fetchNotes.test.js
+++ src/ulit/fetchNotes.test.js
@@ -1,7 +1,10 @@
 import '@testing-library/jest-dom/extend-expect';
+import { renderHook, act } from '@testing-library/react-hooks';
+import Home from '../appComponents/Home';
 
 import fetch from 'jest-fetch-mock';
 import fetchNotes from './fetchNotes'
+import { useState } from 'react';
 
 fetch.enableMocks();
 
@@ -10,15 +13,18 @@ fetch.enableMocks();
 it('should get take a function and get the object', async (/* args */) => {
     // function fetches the url and return object
 
-    fetch.mockResponseOnce()
+    fetch.mockResponseOnce(JSON.stringify({
+        "notes": [
+            {"title": "notes", "body": "note...", "id": 1},
+            {"title": "notes", "body": "note...", "id": 2},
+            {"title": "notes", "body": "note...", "id": 3}
+        ]
+    }))
 
     const response = await fetchNotes();
 
-    expect(response).toEqual(expect.any(Object))
-
+    expect(response).toEqual(expect.any(Object));   
     
 });
 
-// test hook
-// see if the setNotes hook map outs the notes
 
