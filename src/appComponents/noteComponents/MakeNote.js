1555f0437ed3f0ea9f4318873170bba5b46f1c8c
Roody <rjointer2@gmail.com>
2021-04-19 18:03:11 -0700

    fixed the editor, save method was incorrect

--- src/appComponents/noteComponents/MakeNote.js
+++ src/appComponents/noteComponents/MakeNote.js
@@ -1,14 +1,55 @@
-
-import editor from '../../ulit/editorTemplate';
-import noteFunctions from '../../ulit/__noteFunctions';
+import EditorJs from '@editorjs/editorjs';
+import Header from '@editorjs/header';
+import List from '@editorjs/list';
+import Embed from '@editorjs/embed';
 
 const MakeNote = () => {
 
+        
+    const editor = new EditorJs({
+
+        // Where the content is outputted
+        holder: 'editorjs',
+        placeholder: 'Let`s write an awesome story!',
+
+        tools: {
+            header: {
+                class: Header,
+                inlineToolbar: ['link']
+            }
+        },
+        list: {
+            class: List,
+            inlineToolbar: [
+                'link',
+                'bold'
+            ]
+        },
+        embed: {
+            class: Embed,
+            inlineToolbar: false,
+            config: {
+                services: {
+                    youtube: true,
+                    coub: true
+                }
+            }
+        }
+
+    });
+
+const saveNote = () => {
+    editor.save().then((outputData) => {
+    console.log('Article data: ', outputData)
+    }).catch((error) => {
+    console.log('Saving failed: ', error)
+    });
+}
     
     return (
         <div>
             <div id="editorjs"></div>
-            <button className="btn" onClick={noteFunctions.saveNote}>
+            <button className="btn" onClick={saveNote}>
                 Save Note!
             </button>
         </div>
